45245c6b9a4d35ff8ca6136b55baa7fe133a4e61
luosen <heyrosen@163.com>
2023-04-21 15:51:59 +0800

    删除无用代码

--- service/src/auth_service/authorization.rs
+++ service/src/auth_service/authorization.rs
@@ -105,16 +105,6 @@ impl AuthServiceApi for AuthService {
     }
 }
 
-// fn check_auth_header(auth_header: Option<&str>) -> Result<String, InternalError> {
-//     if let Some(auth_string) = auth_header {
-//         let vec_header: Vec<&str> = auth_string.split_whitespace().collect();
-//         if vec_header.len() == 2 && vec_header[0] == "Bearer" {
-//             return Ok(vec_header[1].to_string());
-//         }
-//     }
-//     Err(InternalError::InvalidAuthToken("Token错误".to_string()))
-// }
-
 #[async_trait]
 impl FromRequestParts<AppState> for AuthorizedUser
 where
