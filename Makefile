417670d591220719442359a083d4e9ea1dcbfe83
luosen <heyrosen@163.com>
2023-05-19 19:29:14 +0800

    成功运行&部署到阿里云

--- Makefile
+++ Makefile
@@ -3,4 +3,5 @@
 
 build-fc:
 	@TARGET_CC=x86_64-linux-musl-gcc cargo build --release --target x86_64-unknown-linux-musl
-	@cp target/x86_64-unknown-linux-musl/release/openai-http-proxy aliyun-fc
+	@mkdir -p serverless-fc
+	@cp target/x86_64-unknown-linux-musl/release/openai-http-proxy serverless-fc
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use hyper::client::HttpConnector;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Client, Request, Response, Server};
-use hyper_tls::HttpsConnector;
+use hyper_rustls::HttpsConnector;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
@@ -11,9 +11,13 @@ static OPENAI_URL: &str = "https://api.openai.com";
 
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8100));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
 
-    let https = HttpsConnector::new();
+    let https = hyper_rustls::HttpsConnectorBuilder::new()
+        .with_native_roots()
+        .https_only()
+        .enable_http1()
+        .build();
     let client = Client::builder().build::<_, hyper::Body>(https);
 
     let make_service = make_service_fn(move |_| {
@@ -21,29 +25,34 @@ async fn main() {
         async move { Ok::<_, Infallible>(service_fn(move |req| proxy(client.clone(), req))) }
     });
 
-    let server = Server::bind(&addr)
-        .http1_preserve_header_case(true)
-        .http1_title_case_headers(true)
-        .serve(make_service);
+    let server = Server::bind(&addr).serve(make_service);
 
     println!("Listening on {}", addr);
 
     if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
+        println!("server error: {}", e);
     }
 }
 
-async fn proxy(
-    _client: HttpClient,
-    mut req: Request<Body>,
-) -> Result<Response<Body>, hyper::Error> {
+async fn proxy(_client: HttpClient, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     let path = req.uri().path().to_string();
     let target_url = format!("{}{}", OPENAI_URL, path);
 
-    *req.uri_mut() = target_url.parse().unwrap();
+    let mut request_builder = Request::builder().method(req.method()).uri(target_url);
 
-    _client.request(req).await.map_err(|err| {
-        println!("Error: {}", err);
-        err
-    })
+    if req.headers().contains_key("Authorization") {
+        request_builder = request_builder.header(
+            hyper::header::AUTHORIZATION,
+            req.headers().get("Authorization").unwrap().to_owned(),
+        );
+    }
+
+    request_builder = request_builder.header(
+        hyper::header::CONTENT_TYPE,
+        hyper::header::HeaderValue::from_static("application/json"),
+    );
+
+    let new_request = request_builder.body(req.into_body()).unwrap();
+
+    _client.request(new_request).await
 }
